03671cde733b1a18df7c1b7f88964a5ea218a39a
Ryunosuke Hayashi <ryunosuke.engineer@gmail.com>
2023-01-05 23:24:02 +0900

    一旦保存

--- gin/backend/main.go
+++ gin/backend/main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-  r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong",
-    })
-  })
-  r.Run()
+	r := gin.Default()
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello world",
+		})
+	})
+	r.Run()
 }
